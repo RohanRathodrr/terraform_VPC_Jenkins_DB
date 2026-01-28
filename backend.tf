5195e80f56d234a32ebbae1fa6f3d36ffba15f1c
RohanRathodrr <masterrr143@gmail.com>
2026-01-28 21:03:23 +0530

    bucket name and server key changed

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {       # This will store your terraform.tfstate file in S3 bucket
-    bucket = "jenkins-server-terraform-tfstate-file-bucket"   # Bucket name
+    bucket = "jenkins-terraform-tfstate-file-bucket"   # Bucket name
     key    = "jenkins/terraform.tfstate" # this creates ours directory and stores terraform.tfstate file in that directory
     region = "ap-south-1"  # region in which your data is stored
   }
